57317545abc7e0819aff70b6ba7dcac9da2d5306
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2023-01-14 02:34:20 +0200

    add example

--- example/just_either_example.dart
+++ example/just_either_example.dart
@@ -1,6 +1,44 @@
 import 'package:just_either/just_either.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> args) async {
+  final data = await getData();
+  data.fold((failure) => print(failure.message), (data) => print(data));
+  print('done');
+}
+
+Future<Either<Failure, String>> getData() async {
+  final clint = HttpClient();
+  try {
+    final data = await clint
+        .getUrl(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'))
+        .then((req) => req.close())
+        .then((res) => res.transform(utf8.decoder).join());
+    return Right(data);
+  } on Exception {
+    return Left(NetworkFailure(message: 'network failure'));
+  } finally {
+    clint.close();
+  }
+}
+
+abstract class Failure {
+  Failure({required this.message});
+  final String message;
+
+  @override
+  bool operator ==(covariant Failure other) {
+    if (identical(this, other)) return true;
+
+    return other.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+}
+
+class NetworkFailure extends Failure {
+  NetworkFailure({required super.message});
 }
